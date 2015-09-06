eef725f8350381d6c3df18b6e7ae3d36bcb6f3e1
Richard Habeeb <richard.habeeb@gmail.com>
2015-09-06 16:13:50 -0500

    Basic scoreboard implemented. Ball resetting.

--- pong.js
+++ pong.js
@@ -28,17 +28,15 @@ function vector(setX, setY) {
         y: setY
     };
 
-    that.angle = function () {
-
-    }
-
-    that.length = function () {
-
-    }
-
     return that;
 }
 
+var gameState = {
+    running: 0,
+    leftScored: 1,
+    rightScored: 2,
+    paused: 3,
+};
 var pixelsPerMeter = 10;
 var defaultBallSpeed = 20;
 var defaultBallMass = 1;
@@ -61,16 +59,18 @@ function ball (setPosition) {
         boardElement.appendChild(domElement);
     }
 
-    that.updateLocation = function(elapsedTimeSeconds, boardSize) {
+    that.detachFrom = function (boardElement) {
+        boardElement.removeChild(domElement);
+    }
+
+    that.updateAndCheckLocation = function(elapsedTimeSeconds, boardSize) {
         that.position.x += that.velocity.x * elapsedTimeSeconds;
         that.position.y += that.velocity.y * elapsedTimeSeconds;
         if(that.position.x > (boardSize.x - that.radius)) {
-            that.position.x = boardSize.x - that.radius;
-            that.velocity.x = -that.velocity.x;
+            return gameState.leftScored;
         }
         if(that.position.x < that.radius) {
-            that.position.x = that.radius;
-            that.velocity.x = -that.velocity.x;
+            return gameState.rightScored;
         }
         if(that.position.y > (boardSize.y - that.radius)) {
             that.position.y = boardSize.y - that.radius;
@@ -80,6 +80,7 @@ function ball (setPosition) {
             that.position.y = that.radius;
             that.velocity.y = -that.velocity.y;
         }
+        return gameState.running;
     }
 
     return that;
@@ -138,12 +139,73 @@ function player (setPosition) {
     return that;
 }
 
-function artificialPlayer (controlledPlayer, quaffle) {
+function artificialPlayer (controlledPlayer) {
     var that = {};
 
-    that.update = function () {
+    that.update = function (quaffle) {
         if(quaffle.position.y < controlledPlayer.position.y) controlledPlayer.appliedForce.y = -100;
         if(quaffle.position.y > controlledPlayer.position.y) controlledPlayer.appliedForce.y = 100;
+    }
+
+    return that;
+}
+
+function scoreText (setPosition) {
+    var that = {};
+    var score = 0;
+    var domElement = createSvgUtl('text');
+
+    that.position = vector(setPosition.x, setPosition.y);
+
+    that.draw = function() {
+        domElement.setAttribute('x', that.position.x * pixelsPerMeter);
+        domElement.setAttribute('y', that.position.y * pixelsPerMeter);
+        domElement.setAttribute('font-size', 5 * pixelsPerMeter);
+        domElement.setAttribute('font-family', 'Roboto');
+        domElement.setAttribute('font-weight', 900);
+        domElement.setAttribute('fill', '#aaaaaa');
+        domElement.innerHTML = score;
+    }
+
+    that.attachTo = function (boardElement) {
+        boardElement.appendChild(domElement);
+    }
+
+    that.increment = function() {
+        score++;
+    }
+
+    return that;
+}
+
+var defaultScoreBoardSize = vector(30, 11);
+function scoreBoard (setPosition) {
+    var that = {}
+    that.position = vector(setPosition.x, setPosition.y);
+    that.currentSize = vector(defaultScoreBoardSize.x, defaultScoreBoardSize.y);
+    var leftScore = scoreText(vector(10, that.currentSize.y - 1));
+    var rightScore = scoreText(vector(20, that.currentSize.y - 1));
+    var domElement = createSvgUtl('svg');
+
+    that.draw = function() {
+        domElement.setAttribute('width', that.currentSize.x * pixelsPerMeter);
+        domElement.setAttribute('height', that.currentSize.y * pixelsPerMeter);
+        domElement.setAttribute('x', (that.position.x - (that.currentSize.x / 2)) * pixelsPerMeter);
+        domElement.setAttribute('y', (that.position.y - (that.currentSize.y)) * pixelsPerMeter);
+        domElement.setAttribute('fill', 'none');
+        leftScore.draw();
+        rightScore.draw();
+    }
+
+    that.attachTo = function (boardElement) {
+        leftScore.attachTo(domElement);
+        rightScore.attachTo(domElement);
+        boardElement.appendChild(domElement);
+    }
+
+    that.addScore = function(state) {
+        if(state == gameState.leftScored) leftScore.increment();
+        if(state == gameState.rightScored) rightScore.increment();
     }
 
     return that;
@@ -158,21 +220,29 @@ function gameBoard (setSize) {
     var domElement = createSvgUtl('svg');
     var previousTimeStamp = null;
     var quaffle = ball(vector(size.x / 2, 0));
-    var leftPlayerController = artificialPlayer(leftPlayer, quaffle);
-    var rightPlayerController = artificialPlayer(rightPlayer, quaffle);
+    var leftPlayerController = artificialPlayer(leftPlayer);
+    var rightPlayerController = artificialPlayer(rightPlayer);
+    var score = scoreBoard(vector(size.x / 2, defaultScoreBoardSize.y / 2));
 
     var update = function (elapsedTimeSeconds) {
-        leftPlayerController.update();
-        rightPlayerController.update();
+        leftPlayerController.update(quaffle);
+        rightPlayerController.update(quaffle);
         leftPlayer.updateLocation(elapsedTimeSeconds, size);
         rightPlayer.updateLocation(elapsedTimeSeconds, size);
-        quaffle.updateLocation(elapsedTimeSeconds, size);
+        var state = quaffle.updateAndCheckLocation(elapsedTimeSeconds, size);
+        if(state == gameState.leftScored || state == gameState.rightScored) {
+            quaffle.detachFrom(domElement);
+            quaffle = ball(vector(size.x / 2, 0));
+            quaffle.attachTo(domElement);
+            score.addScore(state);
+        }
     }
 
     var render = function () {
         leftPlayer.draw();
         rightPlayer.draw();
         quaffle.draw();
+        score.draw();
     }
 
     var loop = function (timeStamp) {
@@ -191,6 +261,7 @@ function gameBoard (setSize) {
         leftPlayer.attachTo(domElement);
         rightPlayer.attachTo(domElement);
         quaffle.attachTo(domElement);
+        score.attachTo(domElement);
         window.requestAnimationFrame(loop);
     }
 
